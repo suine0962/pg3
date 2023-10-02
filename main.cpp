adee1c7a57a6ab61898c88675e29ca00d60f5299
裕斗 近藤 <hilotono@icloud.com>
2023-10-02 11:20:47 +0900

    1-1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
 #include <stdio.h>
+
+
+template<typename Type>
+
+Type Min(Type a, Type b)
+{
+	if (a <= b)
+	{
+		return a;
+	}
+	if (a >= b)
+	{
+		return b;
+	}
+}
+
+template<>
+char Min<char>(char a, char b)
+{
+	return printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+}
+
+int main()
+{
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(11.4f, 51.4f));
+	printf("%f\n", Min<double>(1.0f, 4.0f));
+	printf("%c\n", Min<char>(1,3));
+
+
+	return 0;
+}
