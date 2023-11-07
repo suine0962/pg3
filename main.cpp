e4865edaa4909f0458d82a882bb5c60088cc3ca5
裕斗 近藤 <hilotono@icloud.com>
2023-11-07 19:41:44 +0900

    0102完

--- main.cpp
+++ main.cpp
@@ -1,65 +1,42 @@
-﻿#include <stdio.h>
-
-
-template<typename Type>
-
-Type Min(Type a, Type b)
-{
-	if (a <= b)
-	{
-		return static_cast<Type>(a);
-	}
-	if (a >= b)
-	{
-		return static_cast<Type>(b);
-	}
-}
-
-template<>
-
-char Min<char>(char a, char b)
-{
-	 printf("数字以外は代入できません");
-	 return 0;
-}
-
+#include<stdio.h>
+#include<Windows.h>
 int GenerallySalary(int hours)
 {
 	int RegularRate = 1072;
 	return RegularRate * hours;
 }
 
-int RecursiveSalary(int hours)
+int RecursiveSalary(int hours,int time)
 {
-	int InitialHourlyRate = 100;
-	int TotalSalary = 0;
-
-	for (int i = 0; i < hours; ++i)
+	if (hours == 0)
 	{
-		TotalSalary += InitialHourlyRate;
-		//次の時給を計算
-		InitialHourlyRate = (TotalSalary * 2) - 50;
+		return 0;
 	}
-	return TotalSalary;
+	else
+	{
+		float currenWage = time * 1;
+		float nextHourlyRate = time * 2 - 50;
+		return currenWage + RecursiveSalary(hours -1 ,nextHourlyRate);
+	}
+
+	
 }
 
 
 int main()
 {
 
-	int hoursWorks;
-	printf("働いた時間を入力してください: ");
-	scanf_s("%d",&hoursWorks);
+	int hoursWorks = 9;
 
+	//一般的な賃金体系
 	int reguLarSalary = GenerallySalary(hoursWorks);
-	int recursiveSalary = RecursiveSalary(hoursWorks);
-	
-	printf("通常の賃金体系での給料:%d円\n", reguLarSalary);
-	printf("再帰的な賃金体系での給料:%d円\n", recursiveSalary);
-	/*printf("%d\n",Min<int>(114, 514));
-	printf("%f\n", Min<float>(11.4f, 51.4f));
-	printf("%f\n", Min<double>(1.0f, 4.0f));
-	Min<char>(1, 3);*/
+	printf("一般的な賃金体系での給与:%d円\n",reguLarSalary);
+
+
+	//再帰的な賃金体系
+	int initilalHourlyRate = 100;
+	int recursiveWage = RecursiveSalary(hoursWorks - 1, initilalHourlyRate);
+	printf("再帰的な賃金体系での給料:%d円\n", recursiveWage);
 
 
 	return 0;
