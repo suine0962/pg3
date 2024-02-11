f6c01a2be5953662647cfc919b9fd912d7823aae
裕斗 近藤 <hilotono@icloud.com>
2024-02-12 03:34:48 +0900

    kan

--- main.cpp
+++ main.cpp
@@ -1,13 +1,38 @@
 ﻿#include <stdio.h>
+#include <iostream>
+#include<vector>
+#include<thread>
 
+using namespace std;
 
-int main()
-{
-	/*printf("%d\n",Min<int>(114, 514));
-	printf("%f\n", Min<float>(11.4f, 51.4f));
-	printf("%f\n", Min<double>(1.0f, 4.0f));
-	Min<char>(1, 3);*/
+int main() {
 
+	const int TextSize = 100000;
+	string TextA(TextSize, 'a');
+	cout << TextSize << "文字のコピーと移動を比較\n";
+
+	chrono::steady_clock::time_point now;
+	chrono::steady_clock::time_point end;
+
+	///copy
+	now = chrono::steady_clock::now();
+	string CopyTextA = TextA;
+	end = chrono::steady_clock::now();
+
+	int copy = chrono::duration_cast<chrono::microseconds>(end - now).count();
+
+	cout << "コピーの場合" << "\n";
+	cout << copy << "\n";
+
+	///move
+	now = chrono::steady_clock::now();
+	string MoveText_A = move(TextA);
+	end = chrono::steady_clock::now();
+
+	int Move = chrono::duration_cast<chrono::microseconds>(end - now).count();
+
+	cout << "移動の場合" << "\n";
+	cout << Move << " \n";
 
 	return 0;
 }
